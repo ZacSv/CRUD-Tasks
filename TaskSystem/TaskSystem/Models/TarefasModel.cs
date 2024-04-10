d70eeeffd563ed9f6ade04c38f99992d938005e5
Isac Vieira <isacvieira2009@hotmail.com>
2024-04-10 15:18:05 -0300

    Modelando controller

---
+++ TaskSystem/TaskSystem/Models/TarefasModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskSystem.Models
+{
+    public class TarefasModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }   
+        public string? Descricao { get; set;}
+        public int Status { get; set; }
+    }
+}
--- TaskSystem/TaskSystem/obj/Debug/net8.0/TaskSystem.MvcApplicationPartsAssemblyInfo.cs
+++ TaskSystem/TaskSystem/obj/Debug/net8.0/TaskSystem.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TaskSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9cb88a19d93f8f19cb3cb11eb58db608149feb2")]
-[assembly: System.Reflection.AssemblyProductAttribute("TaskSystem")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TaskSystem")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
